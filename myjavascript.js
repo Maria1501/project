0fcf19a301af4caf3e3142085ea6caffe3bdbe52
Мария <sheina.maria31@yandex.ru>
2020-07-27 11:52:52 +0500

    first commit

---
+++ myjavascript.js
@@ -0,0 +1,8 @@
+/*test.onclick = function() {
+	alert('Привет');
+}*/
+var say = function(){
+	alert('Привет');
+}
+test.addEventListener('mouseover', say);
+//test.removeEventListener('click',say);
---
+++ script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+a=5;
+console.log(a);
+//alert("Hello world");
+/* let answer = prompt("Есть ли вам 18?", "Да");
+console.log(typeof(answer));
+console.log("arr"+"-object"); */
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+console.log(5%2);
+console.log("2" === 2);
+
+let isChecked = true;
+    isClose = false;
+
+console.log(isChecked || isClose);
